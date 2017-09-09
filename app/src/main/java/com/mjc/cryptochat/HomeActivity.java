c8907a956035ad883394bfedfe67b0c4a7ce7041
Pyozer <jeancharles.msse@gmail.com>
2017-09-09 22:21:40 +0200

    Ajout home

--- app/src/main/java/com/mjc/cryptochat/HomeActivity.java
+++ app/src/main/java/com/mjc/cryptochat/HomeActivity.java
@@ -1,13 +1,37 @@
 package com.mjc.cryptochat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button mHomeRegister;
+    private Button mHomeLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mHomeRegister = findViewById(R.id.home_register);
+        mHomeLogin = findViewById(R.id.home_login);
+
+        mHomeRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        mHomeLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
