b31507d0afc08bc3de3762113f4d5dafc07384a2
bijou <pocard.maxime@gmail.com>
2017-09-09 22:18:59 +0200

    Adding some stuff on the Login screen

--- app/src/main/java/com/mjc/cryptochat/LoginActivity.java
+++ app/src/main/java/com/mjc/cryptochat/LoginActivity.java
@@ -2,12 +2,21 @@ package com.mjc.cryptochat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView login, password;
+    private Button connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (TextView)findViewById(R.id.login);
+        password = (TextView)findViewById(R.id.password);
+
+        connect = (Button)findViewById(R.id.connect);
     }
 }
